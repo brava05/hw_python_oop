5a36c3245e25c0ec314ffab99773a271999a5902
Валерий Бражинский <brava_05@mail.ru>
2022-05-01 16:17:33 +0300

    My commit5

--- homework.py
+++ homework.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from dataclasses import asdict
+from typing import Dict, Type
 
 
 @dataclass
@@ -29,8 +30,8 @@ class Training:
     duration: float
     weight: float
     LEN_STEP: float = 0.65
-    M_IN_KM: int = 1000
-    MIN_IN_H: int = 60
+    M_IN_KM: float = 1000
+    MIN_IN_H: float = 60
 
     def __init__(self,
                  action: int,
@@ -148,6 +149,8 @@ class Swimming(Training):
         """Получить среднюю скорость движения.
         Формула длина_бассейна * count_pool / M_IN_KM / время_тренировки."""
 
+        # в read_package() тоже добавил проверку, но сюда возможно будут
+        # попадать из других мест данные потом )
         if self.duration == 0:
             return 0
         else:
@@ -160,34 +163,40 @@ class Swimming(Training):
         """Получить количество затраченных калорий.
         Формула (средняя_скорость + 1.1) * 2 * вес"""
 
-        return ((self.get_mean_speed() + self.CALORIES_SWIMMING_COEFFICIENT_1)
-                * self.CALORIES_SWIMMING_COEFFICIENT_2
-                * self.weight)
+        return (
+            (
+                self.get_mean_speed()
+                + self.CALORIES_SWIMMING_COEFFICIENT_1
+            )
+            * self.CALORIES_SWIMMING_COEFFICIENT_2
+            * self.weight
+        )
 
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
 
-    dict_training = {
+    dict_training: Dict[str, Type(Training)] = {
         'SWM': Swimming,
         'RUN': Running,
         'WLK': SportsWalking
     }
 
     if workout_type not in dict_training:
-        raise NotImplementedError(
+        raise KeyError(
             'В данных указан вид тренировки, не предусмотренный программой')
 
-    # создаем объект нужного типа
+    if data[1] == 0:
+        raise('Продолжительность тернировки не может быть равно нулю')
+
+    # создаем объект нужного типа (когда-то это будет очевидно из кода,
+    # пока для меня это магия: создать объект по значению какой-то переменной)
     return dict_training[workout_type](*data)
 
 
 def main(training: Training) -> None:
     """Главная функция."""
 
-    # сомневаюсь, что так лучше для понимания кода,
-    # чем с промежуточной переменной,
-    # но замечание учел
     print(training.show_training_info().get_message())
 
 
@@ -199,7 +208,5 @@ if __name__ == '__main__':
     ]
 
     for workout_type, data in packages:
-        # здесь тоже можно убрать одноразовую переменную
-        # но тогда код вообще какой-то неудобоваримый полчается
         training = read_package(workout_type, data)
         main(training)
